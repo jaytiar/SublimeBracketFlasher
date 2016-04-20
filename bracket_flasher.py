020cc7a8d4264cf9d2e107eef6b71d84b66f2ffd
Jon Rowland <jon.rowland@metaswitch.com>
2016-04-20 17:27:25 +0100

    Do analysis based on configurable settings, rather than hard-coding

--- bracket_flasher.py
+++ bracket_flasher.py
@@ -10,11 +10,6 @@ def plugin_loaded():
 
 class BracketFlasher(sublime_plugin.EventListener):
     """Event listener for highlighting bracket matches"""
-    PAIRS = {
-        ")": "(",
-        "]": "[",
-        "}": "{"
-    }
 
     def on_modified(self, view):
         """Called whenever a view is modified."""
@@ -26,46 +21,59 @@ class BracketFlasher(sublime_plugin.EventListener):
         error_style = settings.get("error_scope", "invalid.illegal")
         backtrack_limit = settings.get("backtrack_limit", 100000)
         flash_time = settings.get("flash_time", 200)
+        brackets = settings.get("brackets", [])
 
         start_location = view.sel()[0].begin() - 1
-        scope = view.scope_name(start_location)
-        if "comment" in scope or "string" in scope:
-            return
         bracket = view.substr(start_location)
-        if bracket in self.PAIRS.keys():
-            # We are to the right of a bracket.
-            partner = self.PAIRS[bracket]
+        for b in brackets:
+            if bracket == b["bracket"]:
+                # We are to the right of a bracket.
+                # Check this is a scope we're interested in.
+                scope = view.scope_name(start_location)
+                include = b["include"]
+                exclude = b["exclude"]
+                if (include and not any(i in scope for i in include)) or \
+                   (exclude and any(e in scope for e in exclude)):
+                    # Not a valid scope, try the next one.
+                    continue
 
-            # Count backwards for matching brackets.
-            count = 1
-            location = start_location - 1
-            while count > 0 and location >= 0 \
-                    and (start_location - location) < backtrack_limit:
-                char = view.substr(location)
-                scope = view.scope_name(location)
-                # Ignore non-code brackets.
-                if "comment" not in scope and "string" not in scope:
-                    if char == bracket:
-                        count += 1
-                    elif char == partner:
-                        count -= 1
-                location -= 1
-            location += 1
-            start_location += 1
+                partner = b["partner"]
 
-            # If we got to the start of the file with no match, it's an error.
-            if count != 0:
-                style = error_style
-            else:
-                style = match_style
+                # Count backwards for matching brackets.
+                count = 1
+                location = start_location - 1
+                while count > 0 and location >= 0 \
+                        and (start_location - location) < backtrack_limit:
+                    char = view.substr(location)
+                    scope = view.scope_name(location)
 
-            view.add_regions("BracketFlasher" + str(start_location),
-                             [sublime.Region(location, start_location)],
-                             style)
-            sublime.set_timeout_async(functools.partial(self.clear_regions,
-                                                        view,
-                                                        start_location),
-                                      flash_time)
+                    # Ignore brackets at the wrong scope.
+                    if (not include or any(i in scope for i in include)) and \
+                       (not exclude or not any(e in scope for e in exclude)):
+                        if char == bracket:
+                            count += 1
+                        elif char == partner:
+                            count -= 1
+                    location -= 1
+                location += 1
+                start_location += 1
+
+                # If we got to the start of the file (or limit), it's an error.
+                if count != 0:
+                    style = error_style
+                else:
+                    style = match_style
+
+                # Create the regions and set a timer to clear them again after
+                # the timeout.
+                view.add_regions("BracketFlasher" + str(start_location),
+                                 [sublime.Region(location, start_location)],
+                                 style)
+                sublime.set_timeout_async(functools.partial(self.clear_regions,
+                                                            view,
+                                                            start_location),
+                                          flash_time)
+                break
 
     def clear_regions(self, view, loc):
         view.erase_regions("BracketFlasher" + str(loc))
